bfdd95f4f9d908a1a27d98a5b75a0f76d7e1cb9e
goshva <sha.egor@gmail.com>
2020-04-15 21:52:26 +0300

    redirect to info@buh26

--- public/mail/MailHandler.php
+++ public/mail/MailHandler.php
@@ -1,6 +1,5 @@
 <?php
-//	$owner_email='info@buhuchet26.ru';
-	$owner_email='sha.egor@gmail.com';
+  	$owner_email='info@buhuchet26.ru';
 	//$owner_email='gowlad@yandex.ru';
 	//SMTP server settings	
 	$host = 'ssl://smtp.gmail.com';

3f528c1339080ed3b95a847c9005655b2c696036
goshva <sha.egor@gmail.com>
2020-04-15 21:17:03 +0300

    simple form

--- themes/Umis/static/js/postmail.js
+++ themes/Umis/static/js/postmail.js
@@ -1,27 +1,20 @@
-document.addEventListener('DOMContentLoaded', () => {
-sendmail();
-});
 function sendmail(){
-    const ajaxSend = (formData) => {
-        fetch('/mail/MailHandler.php', { // файл-обработчик 
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded', // отправляемые данные 
-            },
-            body: formData
-        })
-            .then(response => console.log('Сообщение отправлено методом fetch'))
-            .catch(error => console.error(error))
-    };
+var data = new FormData();
+data.append("name", document.getElementById('name'));
+data.append("email", document.getElementById('email'));
+data.append("phone", document.getElementById('phone'));
+data.append("message", document.getElementById('message'));
 
-    const forms = document.getElementsByTagName('form');
-    for (let i = 0; i < forms.length; i++) {
-        forms[i].addEventListener('submit', function (e) {
-            e.preventDefault();
-            const formData = new FormData(this);
-            ajaxSend(formData);
-            this.reset(); // очищаем поля формы 
-        });
-    };
-};
+var xhr = new XMLHttpRequest();
+xhr.withCredentials = true;
+
+xhr.addEventListener("readystatechange", function () {
+  if (this.readyState === 4) {
+    console.log(this.responseText);
+  }
+});
+
+xhr.open("POST", "/mail/MailHandler.php");
+xhr.send(data);
+}
 
